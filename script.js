8997e90bf2d6c1b2406daa6c418255e44356e332
Saidalal <saidalal02@gmail.com>
2024-05-21 15:38:28 +0530

    added delivery status

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
 function checkPincode() {
     var pincode = document.querySelector('.enter-pin').value;
     var pincodeApi = `https://api.postalpincode.in/pincode/${pincode}`;
+    var resultList = document.querySelector('.result-list');
     
+    resultList.innerHTML = '';
+
     fetch(pincodeApi)
         .then(response => response.json())
         .then(data => {
             if (data[0].Status === 'Success') {
-                console.log(`Pincode ${pincode} is valid.`);
+                var postOffices = data[0].PostOffice;
+                postOffices.forEach(postOffice => {
+                    var listItem = document.createElement('li');
+                    listItem.innerHTML = `<b>${postOffice.Name}</b> - ${postOffice.DeliveryStatus}`;
+                    resultList.appendChild(listItem);
+                });
             } else {
-               console.log(`Invalid pincode or pincode not found.`); 
+                var listItem = document.createElement('li');
+                listItem.textContent = 'Invalid pincode or pincode not found.';
+                resultList.appendChild(listItem);
             }
         })
         .catch(error => alert(`Error: ${error}`));
